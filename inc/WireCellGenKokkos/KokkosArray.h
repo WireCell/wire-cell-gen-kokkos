b5f82d0a1cf58f3c61c0f88af19e05721cc323c8
HaiwangYu <yuhw.pku@gmail.com>
2021-05-14 04:22:23 -0400

    init cuda fft

--- inc/WireCellGenKokkos/KokkosArray.h
+++ inc/WireCellGenKokkos/KokkosArray.h
@@ -96,56 +96,13 @@ namespace WireCell {
             return ss.str();
         }
 
-        /**
-         * Below are 4 functions for for the FFT operations.
-         * The actual operation is just a placeholder, adding "1" for debugging purpose.
-         */
-        inline array_xxc dft_rc(const array_xxf& arr, int dim = 0)
-        {
-            Index N0 = arr.extent(0);
-            Index N1 = arr.extent(1);
-            auto ret = gen_2d_view<array_xxc>(N0, N1, 0);
-            // TODO place holder for now, implement a real one!
-            Kokkos::parallel_for("dft_rc",
-                                 Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Left>>({0, 0}, {N0, N1}),
-                                 KOKKOS_LAMBDA(const Index& i0, const Index& i1) { ret(i0, i1) = arr(i0, i1)+1; });
-            return ret;
-        }
-        inline array_xxc dft_cc(const array_xxc& arr, int dim = 1)
-        {
-            Index N0 = arr.extent(0);
-            Index N1 = arr.extent(1);
-            auto ret = gen_2d_view<array_xxc>(N0, N1, 0);
-            // TODO place holder for now, implement a real one!
-            Kokkos::parallel_for("dft_cc",
-                                 Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Left>>({0, 0}, {N0, N1}),
-                                 KOKKOS_LAMBDA(const Index& i0, const Index& i1) { ret(i0, i1) = arr(i0, i1)+1; });
-            return ret;
-        }
-        inline array_xxc idft_cc(const array_xxc& arr, int dim = 1)
-        {
-            Index N0 = arr.extent(0);
-            Index N1 = arr.extent(1);
-            auto ret = gen_2d_view<array_xxc>(N0, N1, 0);
-            // TODO place holder for now, implement a real one!
-            Kokkos::parallel_for("idft_cc",
-                                 Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Left>>({0, 0}, {N0, N1}),
-                                 KOKKOS_LAMBDA(const Index& i0, const Index& i1) { ret(i0, i1) = arr(i0, i1)-1; });
-            return ret;
-        }
-        inline array_xxf idft_cr(const array_xxc& arr, int dim = 0)
-        {
-            Index N0 = arr.extent(0);
-            Index N1 = arr.extent(1);
-            auto ret = gen_2d_view<array_xxf>(N0, N1, 0);
-            // TODO place holder for now, implement a real one!
-            Kokkos::parallel_for("idft_cr",
-                                 Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Left>>({0, 0}, {N0, N1}),
-                                 KOKKOS_LAMBDA(const Index& i0, const Index& i1) { ret(i0, i1) = Kokkos::real(arr(i0, i1))-1; });
-            return ret;
-        }
-
     }  // namespace KokkosArray
 }  // namespace WireCell
+
+#ifdef KOKKOS_ENABLE_CUDA
+#include "WireCellGenKokkos/KokkosArray_cuda.h"
+#else
+#include "WireCellGenKokkos/KokkosArray_dummy.h"
+#endif
 
 #endif
---
+++ inc/WireCellGenKokkos/KokkosArray_cuda.h
@@ -0,0 +1,83 @@
+/**
+ * Similar like the WireCell::Array with Eigen backend,
+ * this KokkosArray provides interface for FFTs.
+ */
+
+#ifndef WIRECELL_KOKKOSARRAY_CUDA
+#define WIRECELL_KOKKOSARRAY_CUDA
+
+#include "WireCellUtil/Waveform.h"
+
+#include <Kokkos_Core.hpp>
+
+#include <cufft.h>
+
+#include <string>
+#include <typeinfo>
+
+namespace WireCell {
+
+    namespace KokkosArray {
+        /**
+         * Below are 4 functions for for the FFT operations.
+         * The actual operation is just a placeholder, adding "1" for debugging purpose.
+         */
+        inline array_xxc dft_rc(const array_xxf& in, int dim = 0)
+        {
+            std::cout << "WIRECELL_KOKKOSARRAY_CUDA" << std::endl;
+            Index N0 = in.extent(0);
+            Index N1 = in.extent(1);
+            auto out = gen_2d_view<array_xxc>(N0, N1, 0);
+
+            cufftHandle plan;
+            // FIXME neede to do 1D FFTs
+            cufftPlan2d(&plan, N0, N1, CUFFT_R2C);
+            cufftExecR2C(plan, (cufftReal*) in.data(), (cufftComplex*) out.data());
+            cufftDestroy(plan);
+
+            return out;
+        }
+        inline array_xxc dft_cc(const array_xxc& in, int dim = 0)
+        {
+            Index N0 = in.extent(0);
+            Index N1 = in.extent(1);
+            auto out = gen_2d_view<array_xxc>(N0, N1, 0);
+            
+            cufftHandle plan;
+            cufftPlan2d(&plan, N0, N1, CUFFT_C2C);
+            cufftExecC2C(plan, (cufftComplex*) in.data(), (cufftComplex*) out.data(), CUFFT_FORWARD);
+            cufftDestroy(plan);
+
+            return out;
+        }
+        inline array_xxc idft_cc(const array_xxc& in, int dim = 0)
+        {
+            Index N0 = in.extent(0);
+            Index N1 = in.extent(1);
+            auto out = gen_2d_view<array_xxc>(N0, N1, 0);
+            
+            cufftHandle plan;
+            cufftPlan2d(&plan, N0, N1, CUFFT_C2C);
+            cufftExecC2C(plan, (cufftComplex*) in.data(), (cufftComplex*) out.data(), CUFFT_INVERSE);
+            cufftDestroy(plan);
+
+            return out;
+        }
+        inline array_xxf idft_cr(const array_xxc& in, int dim = 0)
+        {
+            Index N0 = in.extent(0);
+            Index N1 = in.extent(1);
+            auto out = gen_2d_view<array_xxf>(N0, N1, 0);
+            
+            cufftHandle plan;
+            cufftPlan2d(&plan, N0, N1, CUFFT_C2R);
+            cufftExecC2R(plan, (cufftComplex*) in.data(), (cufftReal*) out.data());
+            cufftDestroy(plan);
+
+            return out;
+        }
+
+    }  // namespace KokkosArray
+}  // namespace WireCell
+
+#endif
